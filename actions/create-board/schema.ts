8bf71b454c1b0cd082bb9768af3dc4ccb60a93e8
Anurag <dev.anuragkr@gmail.com>
2023-12-07 19:34:06 +0530

    server action to create boards

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: 'Title is required',
+      invalid_type_error: 'Title must be a string',
+    })
+    .min(3, {
+      message: 'Title is too short',
+    }),
+});
