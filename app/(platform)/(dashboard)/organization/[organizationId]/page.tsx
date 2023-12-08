c7ab1f1867546d5beed0e5ca90035984d0899d40
Anurag <dev.anuragkr@gmail.com>
2023-12-09 01:01:46 +0530

    added: skeleton loading

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from '@/components/ui/separator';
 import { Info } from './_components/info';
 import BoardList from './_components/board-list';
+import { Suspense } from 'react';
 
 function OrganisationPage() {
   return (
@@ -8,7 +9,9 @@ function OrganisationPage() {
       <Info />
       <Separator className='my-4' />
       <div className='px-2 md:px-4'>
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
