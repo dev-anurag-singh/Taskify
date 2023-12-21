4b2b7d46e7b6d775ae19dbab21b6e06b6f11e892
Anurag <dev.anuragkr@gmail.com>
2023-12-21 23:18:59 +0530

    Query Client Provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,11 +1,14 @@
+import Provider from '@/components/Provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'react-hot-toast';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <Toaster position='bottom-center' toastOptions={{ duration: 3000 }} />
-      {children}
+      <Provider>
+        <Toaster position='bottom-center' toastOptions={{ duration: 3000 }} />
+        {children}
+      </Provider>
     </ClerkProvider>
   );
 }
