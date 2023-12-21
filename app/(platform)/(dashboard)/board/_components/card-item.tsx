7065595ac9f367af6f243ac0c8eda14fbe987a4d
Anurag <dev.anuragkr@gmail.com>
2023-12-21 23:19:54 +0530

    card modal

--- app/(platform)/(dashboard)/board/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/_components/card-item.tsx
@@ -2,6 +2,10 @@
 
 import { Card } from '@prisma/client';
 import { Draggable } from '@hello-pangea/dnd';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { useQuery } from '@tanstack/react-query';
+import { CardWithList } from '@/types';
+import { fetcher } from '@/lib/fetcher';
 
 // import { useCardModal } from '@/hooks/use-card-modal';
 
@@ -11,21 +15,29 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
-  //   const cardModal = useCardModal();
+  const { data: card } = useQuery<CardWithList>({
+    queryKey: ['card', data.id],
+    queryFn: () => fetcher(`/api/cards/${data.id}`),
+  });
 
   return (
-    <Draggable draggableId={data.id} index={index}>
-      {provided => (
-        <div
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          role='button'
-          className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'
-        >
-          {data.title}
-        </div>
-      )}
-    </Draggable>
+    <Dialog>
+      <Draggable draggableId={data.id} index={index}>
+        {provided => (
+          <DialogTrigger asChild>
+            <div
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+              role='button'
+              className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'
+            >
+              {data.title}
+            </div>
+          </DialogTrigger>
+        )}
+      </Draggable>
+      <DialogContent>{card?.title}</DialogContent>
+    </Dialog>
   );
 };
