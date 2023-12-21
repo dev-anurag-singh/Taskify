9fe32d1f8f55926d52278424b2be132d717e7633
Anurag <dev.anuragkr@gmail.com>
2023-12-21 23:49:01 +0530

    delete card server action

---
+++ actions/delete-card/index.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { revalidatePath } from 'next/cache';
+
+import { db } from '@/lib/db/db';
+import { createSafeAction } from '@/lib/create-safe-action';
+
+import { DeleteCard } from './schema';
+import { InputType, ReturnType } from './types';
+// import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    card = await db.card.delete({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to delete.',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
