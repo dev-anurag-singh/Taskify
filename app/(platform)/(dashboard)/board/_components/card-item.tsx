a78911536170dfc130b5d9d8ec3fe1b0ceb5f46e
Anurag <dev.anuragkr@gmail.com>
2023-12-22 23:56:18 +0530

    modified

--- app/(platform)/(dashboard)/board/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/_components/card-item.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card } from '@prisma/client';
+import { AuditLog, Card } from '@prisma/client';
 import { Draggable } from '@hello-pangea/dnd';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { useQuery } from '@tanstack/react-query';
@@ -9,6 +9,7 @@ import { fetcher } from '@/lib/fetcher';
 import { Header } from '@/components/card-modal/header';
 import { Description } from '@/components/card-modal/description';
 import { Actions } from '@/components/card-modal/actions';
+import { Activity } from '@/components/card-modal/activity';
 
 // import { useCardModal } from '@/hooks/use-card-modal';
 
@@ -21,6 +22,10 @@ export const CardItem = ({ data, index }: CardItemProps) => {
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ['card', data.id],
     queryFn: () => fetcher(`/api/cards/${data.id}`),
+  });
+  const { data: cardAuditData } = useQuery<AuditLog>({
+    queryKey: ['card-logs', data.id],
+    queryFn: () => fetcher(`/api/cards/${data.id}/logs`),
   });
 
   return (
@@ -49,6 +54,11 @@ export const CardItem = ({ data, index }: CardItemProps) => {
                 <Description.Skeleton />
               ) : (
                 <Description data={cardData} />
+              )}
+              {!cardAuditData ? (
+                <Activity.Skeleton />
+              ) : (
+                <Activity items={cardAuditData} />
               )}
             </div>
           </div>
