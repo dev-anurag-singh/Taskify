07693715fc76d2b6a60e655f0a78d210506c4920
Anurag <dev.anuragkr@gmail.com>
2023-12-23 00:27:07 +0530

    created activity page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+
+import { Separator } from '@/components/ui/separator';
+
+// import { Info } from '../_components/info';
+
+import { ActivityList } from '../_components/activity-list';
+
+const ActivityPage = async () => {
+  return (
+    <div className='w-full'>
+      <Separator className='my-2' />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList />
+      </Suspense>
+    </div>
+  );
+};
+
+export default ActivityPage;
